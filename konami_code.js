4aca94e0f24d68ae5e41f93529026767e4cbb127
tal shachar <tal.shachar@buzzfeed.com>
2017-11-20 01:44:54 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init(let index = 0;
+function init() {
+let index = 0;
 const key = parseInt(e.detail || e.which);
 
   if (key === code[index]) {
@@ -15,6 +16,5 @@ const key = parseInt(e.detail || e.which);
     index = 0;
   }
 }// Write your JavaScript code inside the init() function
-) {
 
 }
